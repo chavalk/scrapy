43956bf8b477b0b73e96d250a0ea449037c28bcc
chavalk <chavalk@hotmail.com>
2023-06-23 10:29:32 -0500

    Add next page to book spider and add json with scraped data.

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -18,6 +18,7 @@ class BookspiderSpider(scrapy.Spider):
                 book_url = 'https://books.toscrape.com/catalogue/' + relative_url
             yield response.follow(book_url, callback = self.parse_book_page)
 
+        next_page = response.css('li.next a ::attr(href)').get()
         if next_page is not None:
             if 'catalogue/' in next_page:
                 next_page_url = 'https://books.toscrape.com/' + next_page
