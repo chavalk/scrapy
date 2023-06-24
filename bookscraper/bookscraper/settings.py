dfd8ee9b5d4c1cb26499497be0454e784c034a0a
chavalk <chavalk@hotmail.com>
2023-06-24 13:59:12 -0500

    Enable fake user agent middleware.

--- bookscraper/bookscraper/settings.py
+++ bookscraper/bookscraper/settings.py
@@ -58,9 +58,10 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+DOWNLOADER_MIDDLEWARES = {
 #    "bookscraper.middlewares.BookscraperDownloaderMiddleware": 543,
-#}
+   'bookscraper.middlewares.ScrapeOpsFakeUserAgentMiddleware': 400,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
